33e6b83f961556198c99856d5ba8f6cfcbd51083
Sam <samueleguino97@gmail.com>
2022-10-06 19:24:26 -0400

    added dge

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), image()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
